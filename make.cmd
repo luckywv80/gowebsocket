8d261b0971999aa24a58b70d92c6c148b1bd8e87
feng <hggfjt@126.com>
2019-04-22 22:06:12 +0800

    添加web方式

--- make.cmd
+++ make.cmd
@@ -18,28 +18,32 @@ for /f %%k in ('go env GOROOT') do set gr=%%k
  
 
 
-echo step 1/3: create build output directory.
+echo step 1/5: create build output directory.
 IF NOT EXIST bin mkdir bin
  
 
 set GOPROXY=https://goproxy.io 
 
-echo step 2/3: install libs...
+echo step 2/5: install libs...
 go get -u -v github.com/golang/net/websocket
 
 echo %GOPATH%
 
-echo step 3/4: build server.exe   client.exe...
+echo step 3/5: build server.exe   client.exe...
 go build -i -o bin/server.exe src/server.go
 go build -i -o bin/client.exe src/client.go
+go build -i -o bin/server2.exe src/server2.go
 
-echo step 4/4: build server , client ...
+echo step 4/5: build server , client ...
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
 go build -i -o bin/server src/server.go  
 go build -i -o bin/client src/client.go  
+go build -i -o bin/server2 src/server2.go  
 
+echo step 5/5: copy websocket.html
+call copy %pwd%\src\websocket.html %pwd%\bin
 echo build success!
 echo 按任意键结束
 pause>nul
